89a5889f79b20079ab4aaf83c6955f5d5138fe3c
JACE <RJCALL@OUTLOOK.COM>
2020-08-20 11:37:48 -0600

    private ip

--- burntml/settings.py
+++ burntml/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '=-0)2ncyxp6lb&vi5yg=z)5v6n0cr3h3v)b6+5d%thty7e+&n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['172.26.15.124', 'burn-ml.com', '44.233.206.95',  '127.0.0.1']
 
