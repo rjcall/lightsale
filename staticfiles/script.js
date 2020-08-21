96bb526c436b3cbae9153c7948d4db5bc0443404
JACE <RJCALL@OUTLOOK.COM>
2020-08-21 00:30:34 -0600

    private ip

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -1,5 +1,5 @@
 $(function(){
-var col = `<div class="col-2"><img id="flamelogo" src="static/flame.png"></div>`;
+var col = `<div class="col-2"><img id="flamelogo" src="staticfiles/flame.png"></div>`;
 	for (var i = 0; i < 6; i++) {
 		$("#footrow").append(col);
 	}
